1c6a46f9f9c46e6ae2903750ff2fd6ce13c66520
Hacker00002 <abdulbositsharipovv@gmail.com>
2022-10-21 18:12:03 +0500

    finished And

--- js/script.js
+++ js/script.js
@@ -50,11 +50,11 @@ var swiper = new Swiper (".trending-content",{
 
 // scroll-bar
 
-// window.onscroll = function() {mufunction()};
+window.onscroll = function() {mufunction()};
 
-// function mufunction(){
-//     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//     var scrolled = (winScroll / height) * 100;
-//     document.getElementById('scroll-bar').style.width = scrolled + '%';
-// }
+function mufunction(){
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
